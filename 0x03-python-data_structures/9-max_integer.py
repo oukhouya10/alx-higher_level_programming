5b385f30412e77fb55006371b886e36a2123e697
oukhouya10 <mariyam.oukhouya@gmail.com>
2023-11-06 11:45:01 -0800

    8&9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    big_int = my_list[0]
+    for x in range(len(my_list)):
+        if my_list[x] < big_int:
+            big_int = my_list[x]
+    return (big_int)
